b1ebb0be4ed48d75ed88ccf23300ca66f55c3144
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-08-17 22:18:02 -0400

    Changed total calculations

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -4,14 +4,14 @@ import PortfolioTable from './portfolioTable';
 const Portfolio = ({ stocks }) => {
     if(stocks){
         let total = stocks.reduce((sum, item) =>{
-            return sum + parseFloat(item.latestPrice);
+            return sum + (parseFloat(item.latestPrice) * item.quantity);
         }, 0) 
 
         return (
             <div className="row">
                 <div className="col-12 d-flex">
                     <div className="portfolio__title mr-auto">Portfolio Value :</div>
-                    <h2 className="portfolio__money text-right">${total}</h2>
+                    <h2 className="portfolio__money text-right">${Math.floor(total).toFixed(2)}</h2>
                 </div>
                     <PortfolioTable stocks={stocks}/> 
             </div>
